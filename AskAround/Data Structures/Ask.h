05c60fa03dcbbb123a55206e571e8572ff0ac644
Adrian Castillejos <zioyero@gmail.com>
2014-02-21 20:25:37 -0800

    created Ask object

---
+++ AskAround/Data Structures/Ask.h
@@ -0,0 +1,14 @@
+//
+// Created by Adrian Castillejos on 2/21/14.
+// Copyright (c) 2014 Holy Moley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/PFObject+Subclass.h>
+
+
+@interface Ask : PFObject <PFSubclassing>
+
++ (NSString *)parseClassName;
+
+@end
---
+++ AskAround/Data Structures/Ask.m
@@ -0,0 +1,23 @@
+//
+// Created by Adrian Castillejos on 2/21/14.
+// Copyright (c) 2014 Holy Moley. All rights reserved.
+//
+
+#import "Ask.h"
+
+
+@implementation Ask
+{
+
+}
+
+
+#pragma mark Parse
+
++(NSString *)parseClassName
+{
+    return @"Ask";
+}
+
+
+@end
