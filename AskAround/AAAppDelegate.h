d9db71c1230c8c455153a86409193d258edfd83f
Adrian Castillejos <zioyero@gmail.com>
2014-02-21 18:59:49 -0800

    Initial commit

---
+++ AskAround/AAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AAAppDelegate.h
+//  AskAround
+//
+//  Created by Adrian Castillejos on 2/21/14.
+//  Copyright (c) 2014 Holy Moley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AskAround/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  AskAround
+//
+//  Created by Adrian Castillejos on 2/21/14.
+//  Copyright (c) 2014 Holy Moley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AAAppDelegate class]));
+    }
+
+}
