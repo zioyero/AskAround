e73aa9fe6f7a0d7d6d7a684c829294bdedc26706
Agathe Battestini <agathe.battestini@gmail.com>
2014-02-23 01:09:49 -0800

    added viewcontroller to create ask

---
+++ AskAround/ask/AACreateAskViewController.h
@@ -0,0 +1,13 @@
+//
+//  AACreateAskViewController.h
+//  AskAround
+//
+//  Created by Agathe Battestini on 2/23/14.
+//  Copyright (c) 2014 Holy Moley. All rights reserved.
+//
+
+
+
+@interface AACreateAskViewController : UIViewController
+
+@end
---
+++ AskAround/ask/AACreateAskViewController.m
@@ -0,0 +1,53 @@
+//
+//  AACreateAskViewController.m
+//  AskAround
+//
+//  Created by Agathe Battestini on 2/23/14.
+//  Copyright (c) 2014 Holy Moley. All rights reserved.
+//
+
+#import "AACreateAskViewController.h"
+
+@interface AACreateAskViewController ()
+
+//@property
+@end
+
+@implementation AACreateAskViewController
+
+
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.title = @"Ask About";
+
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
