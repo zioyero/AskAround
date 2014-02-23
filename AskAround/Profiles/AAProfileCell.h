8d1e5679fc4efb2b85d53ed625e5c6158ca19a80
Agathe Battestini <agathe.battestini@gmail.com>
2014-02-22 19:05:49 -0800

    cell for the profile view

---
+++ AskAround/Profiles/AAProfileCell.h
@@ -0,0 +1,17 @@
+//
+//  AAProfileCell.h
+//  AskAround
+//
+//  Created by Agathe Battestini on 2/22/14.
+//  Copyright (c) 2014 Holy Moley. All rights reserved.
+//
+
+
+
+@interface AAProfileCell : UITableViewCell
+
+@property (nonatomic, strong) id object;
+
+- (void)setObject:(id)object;
+
+@end
---
+++ AskAround/Profiles/AAProfileCell.m
@@ -0,0 +1,35 @@
+//
+//  AAProfileCell.m
+//  AskAround
+//
+//  Created by Agathe Battestini on 2/22/14.
+//  Copyright (c) 2014 Holy Moley. All rights reserved.
+//
+
+#import "AAProfileCell.h"
+
+@implementation AAProfileCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (void)setObject:(id)object {
+    _object = object;
+}
+
+
+@end
+
